1f90c7bf6204d4d936c3704445246dce54ced80a
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-05-17 09:15:50 +0200

    Pruebas para el juego dos, ninguna funciona ¿porqué?.

--- app/src/main/java/org/lopez/fernando/cajacolores/SplitView.java
+++ app/src/main/java/org/lopez/fernando/cajacolores/SplitView.java
@@ -10,25 +10,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import java.util.Random;
+
 public class SplitView extends AppCompatActivity {
 
     private static int colores[] = { R.color.aguamarina, R.color.cian, R.color.dukeBlue, R.color.UCLABlue, R.color.verdeEsmeralda, R.color.black, R.color.white};
     private int indexColor = 0;
+    private Random r = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_split_view);
     }
     public void dividir(View view) {
-        LinearLayout padre = (LinearLayout)view;
+        LinearLayout padre = (LinearLayout)findViewById( view.getId());
         LinearLayout hijo1 = new LinearLayout(this);
+        hijo1.setId( newId());
         LinearLayout hijo2 = new LinearLayout(this);
+        hijo2.setId( newId());
         if ( padre.getOrientation() == LinearLayout.VERTICAL) {
             Log.d("SplitView_dividir", "Vertical");
 //        android:layout_width="match_parent"
 //        android:layout_height="0dp"
 //        android:layout_weight="1"
-            LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, 10, 1F);
+            LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, 0, 1F);
 
 //        android:orientation="horizontal"
             hijo1.setOrientation( LinearLayout.HORIZONTAL);
@@ -41,7 +47,7 @@ public class SplitView extends AppCompatActivity {
 //            android:layout_width="0dp"
 //            android:layout_height="match_parent"
 //            android:layout_weight="1"
-            LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams( 10, LinearLayout.LayoutParams.MATCH_PARENT, 1F);
+            LinearLayout.LayoutParams parametros = new LinearLayout.LayoutParams( 0, LinearLayout.LayoutParams.MATCH_PARENT, 1F);
 
 //        android:orientation="vertical"
             hijo1.setOrientation( LinearLayout.VERTICAL);
@@ -69,6 +75,13 @@ public class SplitView extends AppCompatActivity {
             indexColor = 0;
         }
         padre.addView( hijo1);
-//        padre.addView( hijo2);
+        padre.addView( hijo2);
+    }
+    private int newId() {
+        int resultado = -1;
+        do {
+            resultado = r.nextInt(Integer.MAX_VALUE);
+        } while( findViewById( resultado) != null);
+        return resultado;
     }
 }
